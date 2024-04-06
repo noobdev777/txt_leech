e9953af25b8d4748518047efb988fa71f62b9d18
noobdev777 <164876911+noobdev777@users.noreply.github.com>
2024-04-06 16:46:25 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "29953693"
-api_hash = "bc080dadf0a0e8ca544ac842bcfcf2e0"
-bot_token = "7182994277:AAGz3vbjIweEe1qxENkorYiwf99Lk3aXSOA"
+api_id = "29519231"
+api_hash = "d6629be2e5ed991ca1b9bcc33843b0d6"
+bot_token = "6389575211:AAFAmLY9seujsOI3gkfN9IICLRPKfnxe6jo"
